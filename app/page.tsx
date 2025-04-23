d5bf8041087038cd41a37b469da2173179981c65
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-04-23 20:48:13 +0530

    authentication with github OAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import Image from "next/image";
 import { cookies } from "next/headers";
+import AuthButton from "./auth-button";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data: tweets } = await supabase.from("tweets").select();
 
-  return <pre>{JSON.stringify(tweets, null, 2)}</pre>;
+  return (
+    <>
+      <AuthButton />
+      <pre>{JSON.stringify(tweets, null, 2)}</pre>;
+    </>
+  );
 }
