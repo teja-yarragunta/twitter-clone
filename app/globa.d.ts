cdbf6fc0b801219ba3e1f115aa08f97ac2940391
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-04-24 09:03:02 +0530

    type safety

--- app/globa.d.ts
+++ app/globa.d.ts
@@ -1,5 +1,14 @@
 import { Database as DB } from "@/lib/database.types";
 
+type Tweet = DB["public"]["Tables"]["tweets"]["Row"];
+type Profile = DB["public"]["Tables"]["profiles"]["Row"];
+
 declare global {
   type Database = DB;
+  // Custom type for TweetWithAuthor
+  type TweetWithAuthor = Tweet & {
+    author: Profile;
+    likes: number;
+    user_has_liked_tweet: boolean;
+  };
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import AuthButtonServer from "./auth-button-server";
 import { redirect } from "next/navigation";
+import NewTweet from "./new-tweet";
+import Likes from "./likes";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -9,9 +11,19 @@ export default async function Home() {
   const {
     data: { session },
   } = await supabase.auth.getSession();
-  const { data: tweets } = await supabase
+  const { data } = await supabase
     .from("tweets")
-    .select("*, profiles(*)");
+    .select("*, author: profiles(*), likes(user_id)");
+
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      author: Array.isArray(tweet.author) ? tweet.author[0] : tweet.author,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session?.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
 
   if (!session) {
     redirect("/login");
@@ -20,7 +32,17 @@ export default async function Home() {
   return (
     <>
       <AuthButtonServer />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      <NewTweet />
+
+      {tweets?.map((tweet) => (
+        <div key={tweet.id}>
+          <p>
+            {tweet.author.name} {tweet.author.username}
+          </p>
+          <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
+        </div>
+      ))}
     </>
   );
 }
